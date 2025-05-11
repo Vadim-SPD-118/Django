fdcdea6b7e3dcdb6d6323027043de636cd0e4dee
Vadim.F <fedotovvadik@gmail.com>
2025-05-11 23:26:05 +0500

    Edit views and urls

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, Http404
+
 
 DATA = {
     'omlet': {
@@ -7,8 +9,8 @@ DATA = {
         'соль, ч.л.': 0.5,
     },
     'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
+        'макароны, кг': 0.3,
+        'сыр, кг': 0.05,
     },
     'buter': {
         'хлеб, ломтик': 1,
@@ -16,7 +18,6 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
@@ -28,3 +29,31 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+def recipe_view(request, dish_name):
+    # Finding the recipe
+    recipe = DATA.get(dish_name)
+    if recipe is None:
+        raise Http404 (f"Рецепт «{dish_name}» не найден")
+    
+     # Read servings, default 1
+    servings_str = request.GET.get('servings', '1')
+    try:
+        servings = int(servings_str)
+        if servings < 1:
+            servings = 1
+    except ValueError:
+        servings = 1
+
+     # Multiply each quantity by servings
+    scaled_recipe = {}
+    for ingredient, qty in recipe.items():
+        scaled_recipe[ingredient] = qty * servings
+
+     # Generate the context and render the template
+    context = {
+        'recipe': scaled_recipe
+    }
+    return render(request, 'calculator/index.html', context)
+
+
